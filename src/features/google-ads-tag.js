80cdf0bed72f803d30e62f0d476c05a08072664e
Roman Jarmukhametov <108522445+RomanJarmukhametov@users.noreply.github.com>
2023-06-02 07:05:47 +0600

    gtag update

--- src/features/google-ads-tag.js
+++ src/features/google-ads-tag.js
@@ -1,7 +1,7 @@
 (function () {
   const script = document.createElement("script");
   script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=UA-217707068-1";
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-DSKWFDKJR3";
   document.head.appendChild(script);
 
   window.dataLayer = window.dataLayer || [];
@@ -10,5 +10,5 @@
   }
   gtag("js", new Date());
 
-  gtag("config", "UA-217707068-1");
+  gtag("config", "G-DSKWFDKJR3");
 })();
